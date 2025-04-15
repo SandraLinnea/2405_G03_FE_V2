a51404f3c474fac7895ff69c586226f02e349ad7
Sandra Linnea <sandra.b93@hotmail.com>
2025-04-15 16:11:34 +0200

    Error fix phonenumber

--- src/scripts/order.js
+++ src/scripts/order.js
@@ -88,6 +88,12 @@ if (shoppingCart) {
       },
       orderItem: orderItems,
     };
+
+    userData.phonenumber = userData.phonenumber.replace(/\D/g, '');
+    userData.phonenumber = userData.phonenumber.toString();
+    console.log("Slutgiltigt telefonnummer:", userData.phonenumber, typeof userData.phonenumber);
+
+
     console.log(userData);
     //const jwt = sessionStorage.getItem("token");
     const apiUrl = "https://grupp-3.vercel.app/api/orders/";
